b10f730ab9f3c90a36db9b3bc3178ac7496bd30b
Urmi-Jana <janaurmi6@gmail.com>
2023-07-21 15:25:57 +0530

    extracted correct query data from GDCquery

--- main.r
+++ main.r
@@ -9,8 +9,8 @@ any(colnames(data) %in% c("progression_or_recurrence"))
 empty_columns <- colSums(is.na(data)) == 0
 data2 <- data[, !empty_columns]
 
-data2 <- data2[, c('submitter_id','progression_or_recurrence')]
-data2 <- data2[rowSums(is.na(data2)) == 0,]
+# data2 <- data2[, c('submitter_id','progression_or_recurrence')]
+# data2 <- data2[rowSums(is.na(data2)) == 0,]
 
 query <- GDCquery(
     project = "CPTAC-3",
@@ -19,16 +19,16 @@ query <- GDCquery(
     workflow.type = "STAR - Counts",
     data.type = "Gene Expression Quantification",
     access = "open",
-    # barcode = data2[data2$submitter_id],
 )
 
-
 output_query <- (getResults(query))
+
+data <- GDCprepare(gliomas_query, summarizedExperiment = TRUE)
 # output <- apply(output_query,2,as.character)
 # # print(output)
 # print(length(output))
 
-samples <- output_query$cases[1:10]
+# samples <- output_query$cases[1:20]
 
 gliomas_query <- GDCquery(
     project = "CPTAC-3",
@@ -41,7 +41,15 @@ gliomas_query <- GDCquery(
 )
 
 output <- getResults(gliomas_query)
-# write.table(output, file="res.csv", sep = ",", row.names=FALSE)
 length(samples)
 
 # GDCdownload(gliomas_query)
+gliomas_data <- GDCprepare(gliomas_query, summarizedExperiment = TRUE)
+
+df <- gliomas_data[gliomas_data$primary_diagnosis=='Glioblastoma']
+
+# count matrix
+gliomas_data_assay <- assay(gliomas_data, 'unstranded')
+# write.table(gliomas_data_assay, file="assay.csv", sep = ",", row.names=FALSE)
+
+all("C3L-02544" %in% data$submitter_id)
